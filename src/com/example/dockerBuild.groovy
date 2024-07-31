b7a1ce85da1b1a881d4143d2a2c0411f8a958be0
Rahul Gupta <iamrahulgupta4u@gmail.com>
2024-07-31 08:59:05 +0530

    build docker Image class

---
+++ src/com/example/dockerBuild.groovy
@@ -0,0 +1,18 @@
+#!/usr/bin/env groovy
+
+
+class Docker implements   Serializable{
+
+    def script
+    Docker (script){
+        this.script=script;
+    }
+    def dockerBuildImage(String imageName){
+        script.echo "building the docker image..."
+        script.withCredentials([script.usernamePassword(credentialsId: 'docker-hub', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+            script.sh "docker build -t $imageName ."
+            script.sh "echo '${script.PASS}' | docker login -u '${script.USER}' --password-stdin"
+            script.sh "docker push $imageName"
+        }
+    }
+}
--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,10 +1,5 @@
 #!/usr/bin/env groovy
 
-def call(){
-    echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'docker-hub', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t iamrahul4u/java-maven-app-devops:1.0 .'
-        sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push iamrahul4u/java-maven-app-devops:1.1'
-    }
+def call(String imageName){
+    return new Docker(this).dockerBuildImage(imageName)
 }
